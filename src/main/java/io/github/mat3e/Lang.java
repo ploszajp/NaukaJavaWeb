3ee9c581ecb1e7a58f9aef5ac339a5c7cd702b96
frv <ploszaj.piotr@gmail.com>
2020-02-16 17:05:24 +0100

    Dodanie POJO - plain old data object

---
+++ src/main/java/io/github/mat3e/Lang.java
@@ -0,0 +1,32 @@
+package io.github.mat3e;
+
+class Lang {
+    private Long id;
+    private String welcomeMsg;
+    private String code;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getWelcomeMsg() {
+        return welcomeMsg;
+    }
+
+    public void setWelcomeMsg(String welcomeMsg) {
+        this.welcomeMsg = welcomeMsg;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+    
+}
